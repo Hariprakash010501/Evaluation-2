3806895d2b47c2921cc124b11c533bfe006e8061
Hariprakash010501 <120704241+Hariprakash010501@users.noreply.github.com>
2023-01-04 02:42:52 +0530

    Updated

--- vite-project/src/Routes/MainRoutes.jsx
+++ vite-project/src/Routes/MainRoutes.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
-import Home from '../Components/Home'
+import Navbar from '../Components/Navbar'
+import Home from "../Components/Home"
 import Login from '../Components/Login'
 import ProductDetails from '../Components/ProductDetails'
-import Navbar from '../Components/Navbar'
 import ErrorMessage from '../Components/ErrorMessage'
-const MainRoutes = ({id}) => {
+export const MainRoutes = () => {
   return (
-    <>
     <div>
         <Navbar/>
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/" element={<Login/>}/>
             <Route path="/product/:id" element={<ProductDetails/>}/>
             <Route path="*" element={<ErrorMessage/>}/>
         </Routes>
     </div>
-    </>
   )
 }
-export default MainRoutes
