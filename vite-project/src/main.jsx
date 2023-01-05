7542926bb3c8ec9f4da3a616a3ebefe675427290
Hariprakash010501 <120704241+Hariprakash010501@users.noreply.github.com>
2023-01-05 21:38:28 +0530

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import { AuthContextProvider } from './Contexts/AuthContextProvider'
+import { ContextProvider } from './Contexts/Context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
